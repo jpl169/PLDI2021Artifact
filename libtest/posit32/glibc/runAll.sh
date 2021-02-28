67d447a2c5e0957bd49a0bc399500c662ca2566d
Jay P Lim <jay@n7493-owl312-01.rad.rutgers.edu>
2021-02-28 14:58:58 -0600

    get ready for posit32 libtest

---
+++ libtest/posit32/glibc/runAll.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./Log LogResult.txt &
+./Log2 Log2Result.txt &
+./Log10 Log10Result.txt &
---
+++ libtest/posit32/intel/runAllStaticConst.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./Log Log_SCResult.txt &
+./Log2 Log2_SCResult.txt &
+./Log10 Log10_SCResult.txt &
