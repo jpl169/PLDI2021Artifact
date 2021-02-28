dda780b230c4b664426dab23260bb187eaeb47a3
Jay P Lim <jay@n7493-owl312-01.rad.rutgers.edu>
2021-02-28 15:18:36 -0600

    posit32 libtest bash script

--- runPosit32LibTest-reference.sh
+++ runPosit32LibTest-reference.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-parallelism=8
+parallelism=3
 
 while getopts j: flag
 do
@@ -22,21 +22,16 @@ echo -e "\033[1m\tBuilding done \033[0m"
 echo -e "\033[1m* Performing math library correctness test \033[0m"
 echo -e "\033[1m\tParallelism: $parallelism jobs\033[0m"
 echo -e "\033[1m\t* PWLibm and GLibc math library correctness test \033[0m"
-cd libtest/float/glibc
+cd libtest/posit32/glibc
 make --silent clean
 make --silent
 cat Commands.txt | parallel -j $parallelism
 make clean
 
 echo -e "\033[1m\tResult: \033[0m"
-cat Exp_FGResult.txt
-cat Exp2_FGResult.txt
-cat Exp10_FGResult.txt
-cat Log_FGResult.txt
-cat Log2_FGResult.txt
-cat Log10_FGResult.txt
-cat Sinh_FGResult.txt
-cat Cosh_FGResult.txt
+cat Log_PGResult.txt
+cat Log2_PGResult.txt
+cat Log10_PGResult.txt
 echo -e "\033[1m\tPWLibm and GLibc math library test complete \033[0m"
 
 echo -e "\033[1m\t* PWLibm against Intel math library test \033[0m"
@@ -47,16 +42,9 @@ cat Commands.txt | parallel -j $parallelism
 make clean
 
 echo -e "\033[1m\tResult: \033[0m"
-cat Exp_FIResult.txt
-cat Exp2_FIResult.txt
-cat Exp10_FIResult.txt
-cat Log_FIResult.txt
-cat Log2_FIResult.txt
-cat Log10_FIResult.txt
-cat Sinh_FIResult.txt
-cat Cosh_FIResult.txt
-cat Sinpi_FIResult.txt
-cat Cospi_FIResult.txt
+cat Log_PIResult.txt
+cat Log2_PIResult.txt
+cat Log10_PIResult.txt
 echo -e "\033[1m\tPWLibm against Intel math library test complete \033[0m"
 
 cd ../../..
